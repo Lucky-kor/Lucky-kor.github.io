981d07ac681ac635837d4819543981169385bb16
Jungmin Lee <75019459+Lucky-kor@users.noreply.github.com>
2024-02-21 03:46:17 +0900

    Add files via upload

---
+++ precache-manifest.d516154d1123e13f9b3cc723ba013fef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33d8d94494bfe2a27dbe330523d7be8",
+    "url": "/sw-checkpoint-css-selector/index.html"
+  },
+  {
+    "revision": "343334113f7d487e68a3",
+    "url": "/sw-checkpoint-css-selector/static/css/main.ae83b28d.chunk.css"
+  },
+  {
+    "revision": "ebcb144a43baccd8a9f1",
+    "url": "/sw-checkpoint-css-selector/static/js/2.cfffea70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sw-checkpoint-css-selector/static/js/2.cfffea70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "343334113f7d487e68a3",
+    "url": "/sw-checkpoint-css-selector/static/js/main.630f6120.chunk.js"
+  },
+  {
+    "revision": "575550997ae1b93d830b",
+    "url": "/sw-checkpoint-css-selector/static/js/runtime-main.28bf0327.js"
+  }
+]);
